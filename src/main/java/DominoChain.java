2732c13d8078f247e0f99438539010b44f5358d7
Rumonz <lone_assassin@mail.ru>
2024-04-26 16:43:44 +0300

    Do domino sorter

---
+++ src/main/java/DominoChain.java
@@ -0,0 +1,103 @@
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class DominoChain {
+    public int x;
+    public int y;
+
+    String sign;
+
+    public DominoChain(int x, int y) {
+        this.x = x;
+        this.y = y;
+        sign = "+";
+    }
+
+
+    public static boolean canConnect(ArrayList<Domino> chainOfDomino, Domino domino) {
+        if (chainOfDomino.isEmpty()) {
+            return true;
+        }
+        Domino lastDomino = chainOfDomino.get(chainOfDomino.size() - 1);
+        return lastDomino.getY() == domino.getX();
+    }
+
+    public static ArrayList<Domino> findResult(ArrayList<Domino> dominoArrayList, ArrayList<Domino> chainOfDomino) {
+        if (chainOfDomino.size() == dominoArrayList.size()) {
+            return chainOfDomino;
+        }
+        for (Domino domino : dominoArrayList) {
+            if (!chainOfDomino.contains(domino)) {
+                if (canConnect(chainOfDomino, domino)) {
+                    chainOfDomino.add(domino);
+                    ArrayList<Domino> result = findResult(dominoArrayList, chainOfDomino);
+                    if (result != null) {
+                        return result;
+                    }
+                    chainOfDomino.remove(chainOfDomino.size() - 1);
+                }
+                domino.swapXY();
+                if (canConnect(chainOfDomino, domino)) {
+                    chainOfDomino.add(domino);
+                    ArrayList<Domino> result = findResult(dominoArrayList, chainOfDomino);
+                    if (result != null) {
+                        return result;
+                    }
+                    chainOfDomino.remove(chainOfDomino.size() - 1);
+                }
+                domino.swapXY();
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+//        Domino domino1 = new Domino(2, 6);
+//        Domino domino2 = new Domino(3, 2);
+//        Domino domino3 = new Domino(6, 6);
+//        Domino domino4 = new Domino(3, 5);
+//        Domino domino5 = new Domino(1, 5);
+//        ArrayList<Domino> dominoArrayList1 = new ArrayList<>();
+//        dominoArrayList1.add(domino1);
+//        dominoArrayList1.add(domino2);
+//        dominoArrayList1.add(domino3);
+//        dominoArrayList1.add(domino4);
+//        dominoArrayList1.add(domino5);
+//        ArrayList<Domino> dominoArrayList2 = new ArrayList<>();
+//        dominoArrayList2.add(domino1);
+//        dominoArrayList2.add(domino2);
+//        dominoArrayList2.add(domino3);
+//        dominoArrayList2.add(domino4);
+//        dominoArrayList2.add(domino5);
+
+        Domino domino1 = new Domino(2, 2);
+        Domino domino2 = new Domino(2, 2);
+        Domino domino3 = new Domino(1, 6);
+        Domino domino4 = new Domino(3, 2);
+        Domino domino5 = new Domino(1, 2);
+        Domino domino6 = new Domino(1, 2);
+        Domino domino7 = new Domino(1, 2);
+        Domino domino8 = new Domino(1, 2);
+        ArrayList<Domino> dominoArrayList1 = new ArrayList<>();
+        dominoArrayList1.add(domino1);
+        dominoArrayList1.add(domino2);
+        dominoArrayList1.add(domino3);
+        dominoArrayList1.add(domino4);
+        dominoArrayList1.add(domino5);
+        dominoArrayList1.add(domino6);
+        dominoArrayList1.add(domino7);
+        dominoArrayList1.add(domino8);
+
+
+
+        ArrayList<Domino> result = findResult(dominoArrayList1, new ArrayList<>());
+        if (result == null) {
+            System.out.println("Решения нет");
+        } else {
+            for (Domino domino : result) {
+                System.out.println(domino.getX() +"|"+ domino.getY() +" "+domino.getSign() );
+            }
+        }
+    }
+}
